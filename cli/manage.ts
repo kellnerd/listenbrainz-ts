547868997c6f57c69a99ec8d6f8798645d8b5576
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-01-19 00:24:56 +0100

    feat(cli): Add command to import or preview listens from a JSON file

--- cli/manage.ts
+++ cli/manage.ts
@@ -1,18 +1,32 @@
+import { ListenBrainzClient } from "../client.ts";
 import { formatListen } from "../listen.ts";
-import { readListensFile } from "../utils.ts";
+import { chunk, readListensFile } from "../utils.ts";
 import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 
 export const cli = new Command()
   .name("manage.ts")
   .version("0.5.0")
   .description("Manage your ListenBrainz listens.")
-  .globalEnv("LB_TOKEN=<UUID:string>", "ListenBrainz user token.", {
+  .globalEnv("LB_TOKEN=<UUID>", "ListenBrainz user token.", {
     prefix: "LB_",
+    required: true,
   })
-  .command("list <path:file>", "Display listens from the given JSON file.")
+  .command("import <path:file>", "Import listens from the given JSON file.")
+  .option("-p, --preview", "Show listens instead of submitting them.")
   .action(async (options, path) => {
-    for await (const listen of readListensFile(path)) {
-      console.log(formatListen(listen));
+    const client = new ListenBrainzClient({ userToken: options.token });
+    const listenSource = readListensFile(path);
+    if (options.preview) {
+      for await (const listen of listenSource) {
+        console.log(formatListen(listen));
+      }
+    } else {
+      let count = 0;
+      for await (const listens of chunk(listenSource, 100)) {
+        await client.import(listens);
+        count += listens.length;
+        console.info(count, "listens imported");
+      }
     }
   });
 
