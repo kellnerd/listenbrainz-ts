012118fa946e61e872a27a933b7e85fec0f4c481
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-01-20 00:36:13 +0100

    feat(cli): Submit "<artist> - <title>" metadata string as listen

--- cli/elbisaur.ts
+++ cli/elbisaur.ts
@@ -1,7 +1,11 @@
 import { ListenBrainzClient } from "../client.ts";
-import { formatListen } from "../listen.ts";
+import { formatListen, type Track } from "../listen.ts";
+import { timestamp } from "../timestamp.ts";
 import { chunk, readListensFile } from "../utils.ts";
-import { Command } from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
+import {
+  Command,
+  ValidationError,
+} from "https://deno.land/x/cliffy@v1.0.0-rc.3/command/mod.ts";
 
 export const cli = new Command()
   .name("elbisaur")
@@ -25,19 +29,51 @@ export const cli = new Command()
   .command("import <path:file>", "Import listens from the given JSON file.")
   .option("-p, --preview", "Show listens instead of submitting them.")
   .action(async function (options, path) {
-    const client = new ListenBrainzClient({ userToken: options.token });
     const listenSource = readListensFile(path);
     if (options.preview) {
       for await (const listen of listenSource) {
         console.log(formatListen(listen));
       }
     } else {
+      const client = new ListenBrainzClient({ userToken: options.token });
       let count = 0;
       for await (const listens of chunk(listenSource, 100)) {
         await client.import(listens);
         count += listens.length;
         console.info(count, "listens imported");
       }
+    }
+  })
+  .command("listen <metadata>")
+  .description(`
+    Submit listen for the given track metadata.
+      <metadata> = "<artist> - <title>"
+  `)
+  .option("--at <datetime>", "Time when you started listening.")
+  .option("-p, --preview", "Show listens instead of submitting them.")
+  .action(async function (options, input) {
+    const startTime = timestamp(options.at);
+    if (isNaN(startTime)) {
+      throw new ValidationError(`Invalid date "${options.at}"`);
+    }
+    const trackMatch = input.match(/(?<artist>.+?) -+ (?<title>.+)/);
+    if (trackMatch?.groups) {
+      const track: Track = {
+        artist_name: trackMatch.groups.artist,
+        track_name: trackMatch.groups.title,
+      };
+      if (options.preview) {
+        console.log(formatListen({
+          listened_at: startTime,
+          track_metadata: track,
+        }));
+      } else {
+        const client = new ListenBrainzClient({ userToken: options.token });
+        await client.listen(track, startTime);
+        console.info("Listen submitted");
+      }
+    } else {
+      throw new ValidationError(`Invalid metadata format "${input}"`);
     }
   });
 
