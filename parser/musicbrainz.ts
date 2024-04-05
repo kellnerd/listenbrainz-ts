2ee40570d53a8f38cf158215990eb9adad737423
David Kellner <52860029+kellnerd@users.noreply.github.com>
2024-04-05 19:03:46 +0200

    feat: Calculate timestamps if the end time for a MB release is given
    This is achieved by calculating the start time from the end time and
    the total duration first, which allows the main track to listen mapping
    to stay as straightforward as previously.

--- parser/musicbrainz.ts
+++ parser/musicbrainz.ts
@@ -7,10 +7,12 @@ import {
 } from "jsr:@kellnerd/musicbrainz@^0.1.3/utils/track";
 import type { Listen } from "../listen.ts";
 
-/** Options for the MusicBrainz parser. */
+/** Options for the MusicBrainz parser, either start or end time is required. */
 export interface MusicBrainzListenOptions {
   /** Unix time when the first track was listened to. */
-  startTime: number;
+  startTime?: number;
+  /** Unix time when the last track finished playing. */
+  endTime?: number;
   /** Range of tracks which were listened to, defaults to all. */
   tracks?: TrackRange;
 }
@@ -20,16 +22,32 @@ export function parseMusicBrainzRelease(
   release: Release<"recordings" | "artist-credits">,
   options: MusicBrainzListenOptions,
 ): Listen[] {
+  const { startTime, endTime } = options;
+  assert(
+    startTime && !endTime || !startTime && endTime,
+    "Either start or end time is required and both are mutually exclusive",
+  );
   const trackRange = options.tracks ?? {};
   const targetMedium = trackRange.medium;
+  let totalDurationMs = 0;
   const media = release.media.map((medium) => {
     if (!medium.tracks || (targetMedium && medium.position !== targetMedium)) {
       return { ...medium, tracks: [] };
     }
-    return { ...medium, tracks: filterTrackRange(medium.tracks, trackRange) };
+    const tracks = filterTrackRange(medium.tracks, trackRange);
+    if (!startTime) {
+      const trackLengths = tracks
+        .map((track) => track.length ?? track.recording.length);
+      assert(
+        trackLengths.every((length) => length !== null),
+        "Unknown track length, can not calculate start time of the first listen",
+      );
+      totalDurationMs += (trackLengths as number[]).reduce((a, b) => a + b, 0);
+    }
+    return { ...medium, tracks };
   });
 
-  let { startTime } = options;
+  let trackStartTime = startTime ?? endTime! - totalDurationMs / 1000;
   let lastDurationMs: number | null = 0;
   return media.flatMap((medium) =>
     medium.tracks.map((track) => {
@@ -37,12 +55,12 @@ export function parseMusicBrainzRelease(
         lastDurationMs !== null,
         "Unknown track length, can not calculate start time of the next listen",
       );
-      startTime += lastDurationMs / 1000;
+      trackStartTime += lastDurationMs / 1000;
 
       const { recording } = track;
       const durationMs = track.length ?? recording.length;
       const listen: Listen = {
-        listened_at: Math.round(startTime),
+        listened_at: Math.round(trackStartTime),
         track_metadata: {
           track_name: recording.title,
           artist_name: joinArtistCredit(recording["artist-credit"]),
